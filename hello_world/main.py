2e0273e6f03c715bcb766bf333bcf455d9450ea6
Leagan-Dan <leaganndanut@gmail.com>
2022-05-18 00:08:34 +0300

    b

--- hello_world/main.py
+++ hello_world/main.py
@@ -3,6 +3,17 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
+def app(environ, start_response):
+    """Simplest possible application object"""
+    data = b'Hello, World!\n'
+    status = '200 OK'
+    response_headers = [
+        ('Content-type', 'text/plain'),
+        ('Content-Length', str(len(data)))
+    ]
+    start_response(status, response_headers)
+    return iter([data])
+
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
